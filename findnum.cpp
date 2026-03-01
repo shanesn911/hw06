bab702342192b170add667482f98ced47db90e8f
Shane Nguyen <shane.sn911@gmail.com>
2026-03-01 03:00:43 +0000

    git commit -m "Add findnum.cpp and search.cpp for number finding and searching functionality"

---
+++ findnum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int FindNum(ifstream &ifs);
+
+int main() {
+    ifstream ifs;
+    string fname;
+    cout << "Enter filename: ";
+    cin >> fname;
+    ifs.open(fname);
+
+    int result = FindNum(ifs);
+    ifs.close();
+
+    cout << "The 2nd largest value is: " << result << endl;
+    return 0;
+}
+
+
+int FindNum(ifstream &ifs) {
+    vector<int> nums;
+    int num;
+
+    while (ifs >> num)
+        nums.push_back(num);
+
+    int largest, second;
+    if (nums[0] >= nums[1]) {
+        largest = nums[0];
+        second  = nums[1];
+    } else {
+        largest = nums[1];
+        second  = nums[0];
+    }
+
+    for (int i = 2; i < (int)nums.size(); i++) {
+        if (nums[i] > largest) {
+            second  = largest;
+            largest = nums[i];
+        } else if (nums[i] > second) {
+            second = nums[i];
+        }
+    }
+
+    return second;
+}
---
+++ search.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+// Note: ifstream and ofstream objects
+//       have to be passed in by ref in functions
+// DO NOT change this declaration!
+vector<int> SeqSearchAll(int target, ifstream &ifs);
+
+int main() {
+    ifstream ifs;
+    string fname;
+    cout << "Enter filename: ";
+    cin >> fname;
+    ifs.open(fname);
+
+    int target;
+    cout << "Enter target: ";
+    cin >> target;
+
+    vector<int> returned = SeqSearchAll(target, ifs);
+    ifs.close();
+
+    if (returned[0] == -1)
+        cout << "Target not found!\n";
+    else {
+        cout << "Target found at position(s): ";
+        for (int i = 0; i < (int)returned.size(); i++) {
+            cout << returned[i] << " ";
+        } // for
+        cout << endl;
+    } // else
+
+    return 0;
+}
+
+
+vector<int> SeqSearchAll(int target, ifstream &ifs) {
+    vector<int> nums;
+    vector<int> positions;
+    int num;
+
+    // Read all integers from file into nums
+    while (ifs >> num)
+        nums.push_back(num);
+
+    // Search nums for all occurrences of target
+    for (int i = 0; i < (int)nums.size(); i++) {
+        if (nums[i] == target)
+            positions.push_back(i);
+    } // for
+
+    // If never found, signal with -1
+    if (positions.empty())
+        positions.push_back(-1);
+
+    return positions;
+} // SeqSearchAll
